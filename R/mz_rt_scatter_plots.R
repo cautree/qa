ee317cb98d12a36ddd5e4fd428b14de599918a9c
Yanyan Liu <yliu@dpm2.dpm.bwh.harvard.edu>
2018-06-19 13:48:09 -0400

    qa initial commit

---
+++ R/mz_rt_scatter_plots.R
@@ -0,0 +1,46 @@
+
+get_mz_ = function(s){
+  a=strsplit(s,"_")
+  b= a[[1]][3]
+  return(b)
+}
+
+get_rt_ = function(s){
+  a=strsplit(s,"_")
+  b= a[[1]][4]
+  return(b)
+}
+
+
+get_group_ = function(s){
+  a=strsplit(s,"_")
+  b= a[[1]][1]
+  return(b)
+}
+
+
+
+create_mz_rt_scatter_plots = function(input_df) {
+
+
+  df_data = input_df[,grep("mzid",colnames(input_df))]
+
+
+  df_mzrt = as.data.frame(colnames(df_data))
+
+
+  df_mzrt = df_mzrt %>%
+    mutate(mz=sapply(as.character(df_mzrt$`colnames(df_sample)`), get_mz_) ) %>%
+    mutate(rt = sapply(as.character(df_mzrt$`colnames(df_sample)`), get_rt_) ) %>%
+    mutate(group=sapply(as.character(df_mzrt$`colnames(df_sample)`), get_group_) ) %>%
+    mutate(mz= as.numeric(mz)) %>%
+    mutate(rt = as.numeric(rt))
+
+
+  df_mzrt %>%
+    ggplot(aes(mz, rt, color = group))  +
+    geom_point()+
+    ggtitle("scatter plot of mz and rt")
+
+
+}
