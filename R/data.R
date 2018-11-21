79ad580a63fa36dab4756104e34b77b690ed7eca
Yanyan Liu <yliu@dpm2.dpm.bwh.harvard.edu>
2018-11-21 11:42:40 -0500

    change data format, create pp data frame

--- R/data.R
+++ R/data.R
@@ -1,4 +1,4 @@
-#' metabolites reading with plate_well as id
+#' sample metabolites reading with plate_well as id
 #'
 #' @source modified from https://stattarget.github.io/docs/demo/
 #'
@@ -15,6 +15,22 @@
 "sample_meta_data"
 
 
+
+#' pool plasma metabolites reading with plate_well as id
+#'
+#' @source modified from https://stattarget.github.io/docs/demo/
+#'
+#' @format A data frame with columns:
+#' \describe{
+#'  \item{plate_well}{string in the "0001_0001 format, the last well for plate one will be "0001_0096}
+#'  \item{Eico_mzid_383.182643_4.2555}{ a metabolites produced from the experiment, it is a combination of group, "mzid", mz and retension time}
+
+#' }
+#' @examples
+#' \dontrun{
+#'  pp_meta_data
+#' }
+"pp_meta_data"
 
 
 
