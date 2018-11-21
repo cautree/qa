2a2ba80ce8f5d245919520aa1bf3d06829f668e9
Yanyan Liu <yliu@dpm2.dpm.bwh.harvard.edu>
2018-11-21 12:57:19 -0500

    update missingness comparision

--- R/get_missingness_vs_running_order_comparision.R
+++ R/get_missingness_vs_running_order_comparision.R
@@ -55,7 +55,7 @@ get_missingness_vs_running_order_comparision = function(sample_meta_data, pp_met
 
 
   na_count %>%
-    ggplot2::ggplot(aes(rank, miss, color = subgroup, color = group)) +
+    ggplot2::ggplot(aes(rank, miss, color = subgroup, shape = group)) +
     ggplot2::geom_point() +
     ggplot2::ggtitle("rank vs number of missing and missing percentage") +
     ggplot2::scale_y_continuous(name = expression("missing percentage out of total"),
