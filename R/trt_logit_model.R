3e73ebe2c36f609ea1d370b3ae6360d287c64427
Yanyan Liu <yliu@dpm2.dpm.bwh.harvard.edu>
2018-11-29 12:19:57 -0500

    create method "trt_logit_model"

--- R/trt_logit_model.R
+++ R/trt_logit_model.R
@@ -3,13 +3,37 @@
 #' This function allows you to get median vs running order comparision between sample and pp
 #' @param sample_meta_data, input data frame with metabolites reading.
 #' @param sample_trt_data, input data frame with treatment info.
-#' @param sample_info_data, input data frame with sample plate location.
+#' @param trt, string showing trtment, which is a var i nthe sample trt data
 #' @param df_name, string for data set
 #' @return a cvs files with beta and p value for treatment effects
 #' @export
 
-trt_logit_model = function(sample_meta_data, sample_trt_data, sample_info_data, df_name="Vital") {
+trt_logit_model = function(sample_delta_data, sample_trt_data, trt="fishoilactive", df_name="Vital") {
 
 
+  if(!trt %in% names(sample_trt_data)){
+    stop("there is no such treatment!")
+  }
+
+  sample_trt_data = sample_trt_data %>%
+    dplyr::select(subjectId, trt)
+
+  sample_delta_data_with_trt = sample_delta_data %>%
+    dplyr::left_join( sample_trt_data, by ="subjectId")
+
+
+  logit_report = sample_delta_data_with_trt %>%
+    tidyr::gather(key="meta", value="meta_reading", -subjectId, -fishoilactive) %>%
+    dplyr::group_by(meta) %>%
+    do(broom::tidy( glm( fishoilactive ~ meta_reading, data= .,  family = binomial))) %>%
+    dplyr::filter( term == "meta_reading") %>%
+    dplyr::select(estimate, p.value, meta) %>%
+    dplyr::rename( beta = estimate,
+                   p_value = p.value,
+                   meta_group = meta) %>%
+    dplyr::select( meta_group, beta, p_value) %>%
+    dplyr::arrange(p_value)
+
+  write.csv(logit_report, paste( df_name, "trt_logit_beta_p_value.csv", sep= "_"))
 
 }
