5bec29e04c5d57bf1015259dcc2f278cc97cc1a2
Yanyan Liu <yliu@dpm2.dpm.bwh.harvard.edu>
2018-11-29 12:02:13 -0500

    add data "sample_delta_data"

--- R/trt_logit_model.R
+++ R/trt_logit_model.R
@@ -1,13 +1,14 @@
 #' A trt_logit_model  function
 #'
 #' This function allows you to get median vs running order comparision between sample and pp
-#' @param sample_meta_data, input data frame.
-#' @param pp_meta_data, input data frame.
+#' @param sample_meta_data, input data frame with metabolites reading.
+#' @param sample_trt_data, input data frame with treatment info.
+#' @param sample_info_data, input data frame with sample plate location.
 #' @param df_name, string for data set
-#' @return a ggplot scatter graph
+#' @return a cvs files with beta and p value for treatment effects
 #' @export
 
-trt_logit_model = function(sample_meta_data, sample_trt_data, df_name="Vital") {
+trt_logit_model = function(sample_meta_data, sample_trt_data, sample_info_data, df_name="Vital") {
 
 
 
