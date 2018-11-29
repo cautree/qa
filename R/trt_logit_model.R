968953c4e10aaee244e183d2232f18d4b5fdbb21
Yanyan Liu <yliu@dpm2.dpm.bwh.harvard.edu>
2018-11-29 11:30:47 -0500

    update "sample_trt_data"

---
+++ R/trt_logit_model.R
@@ -0,0 +1,14 @@
+#' A trt_logit_model  function
+#'
+#' This function allows you to get median vs running order comparision between sample and pp
+#' @param sample_meta_data, input data frame.
+#' @param pp_meta_data, input data frame.
+#' @param df_name, string for data set
+#' @return a ggplot scatter graph
+#' @export
+
+trt_logit_model = function(sample_meta_data, sample_trt_data, df_name="Vital") {
+
+
+
+}
