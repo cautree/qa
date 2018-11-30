547c1c3f38b43c1d5d7fb88ebe83e2d4a4855677
Yanyan Liu <yliu@dpm2.dpm.bwh.harvard.edu>
2018-11-30 16:02:26 -0500

    update "trt_logit_model_with_age_gender"

--- R/trt_logit_model_with_age_sex_gender.R
+++ R/trt_logit_model_with_age_sex_gender.R
@@ -1,4 +1,4 @@
-#' A trt_logit_model  function
+#' A trt_logit_model_with_age_gender  function
 #'
 #' This function allows you to get median vs running order comparision between sample and pp
 #' @param sample_meta_data, input data frame with metabolites reading.
@@ -12,12 +12,42 @@
 trt_logit_model_with_age_gender = function(sample_delta_data, sample_trt_data, sample_clin_data, trt="fishoilactive", df_name="Vital") {
 
 
+  if(! "subjectId" %in% names(sample_delta_data)){
+
+    sample_delta_data = sample_delta_data %>%
+      dplyr::rename(subjectId = plate_well)
+
+    sample_trt_data = sample_trt_data %>%
+      dplyr::rename(subjectId = plate_well)
+
+    sample_clin_data =sample_clin_data %>%
+      dplyr::rename(subjectId = plate_well)
+
+  }
+
+
+
   if(!trt %in% names(sample_trt_data)){
     stop("there is no such treatment!")
   }
 
+  if(! "gender" %in% names(sample_clin_data)){
+    sample_clin_data = sample_clin_data %>%
+      dplyr::rename(gender = sex)
+
+  }
+
+
+  if( race %in% names(sample_clin_data)){
+
   sample_clin_data_1 =sample_clin_data %>%
-    dplyr::select(subjectId, age, gender, race)
+    dplyr::select(subjectId, age, gender, race)}
+
+  else{
+    sample_clin_data_1 =sample_clin_data %>%
+      dplyr::select(subjectId, age, gender)}
+
+
 
   sample_trt_data = sample_trt_data %>%
     dplyr::select(subjectId, trt) %>%
@@ -28,13 +58,19 @@ trt_logit_model_with_age_gender = function(sample_delta_data, sample_trt_data, s
   sample_delta_data_with_trt = sample_delta_data %>%
     dplyr::left_join( sample_trt_data, by ="subjectId") %>%
     dplyr::mutate( gender = as.factor(gender),
-                   race = as.factor(race))
+                   race = as.factor(race)) %>%
+    dplyr::select(-subjectId)
 
+  meta_reading = "meta_reading"
+  age = "age"
+  gender = "gender"
+  race = "race"
 
+  if( race %in% names(sample_clin_data)){
   logit_report = sample_delta_data_with_trt %>%
-    tidyr::gather(key="meta", value="meta_reading", -subjectId, -fishoilactive, - age, -gender, - race) %>%
+    tidyr::gather(key="meta", value="meta_reading",  -trt, - age, -sex, - race) %>%
     dplyr::group_by(meta) %>%
-    do(broom::tidy( glm( fishoilactive ~ meta_reading+age+gender+race, data= .,  family = binomial))) %>%
+    do(broom::tidy( glm(  paste(trt, paste(meta_reading, age, gender, race, sep="+" ), sep="~"), data= .,  family = binomial))) %>%
     dplyr::filter( term == "meta_reading") %>%
     dplyr::select(estimate, p.value, meta) %>%
     dplyr::rename( beta = estimate,
@@ -42,6 +78,23 @@ trt_logit_model_with_age_gender = function(sample_delta_data, sample_trt_data, s
                    meta_group = meta) %>%
     dplyr::select( meta_group, beta, p_value) %>%
     dplyr::arrange(p_value)
+  } else{
+
+    logit_report = sample_delta_data_with_trt %>%
+      tidyr::gather(key="meta", value="meta_reading",  -trt, - age, -sex) %>%
+      dplyr::group_by(meta) %>%
+      do(broom::tidy( glm( paste(trt, paste(meta_reading, age, gender, sep="+" ), sep="~"), data= .,  family = binomial))) %>%
+      dplyr::filter( term == "meta_reading") %>%
+      dplyr::select(estimate, p.value, meta) %>%
+      dplyr::rename( beta = estimate,
+                     p_value = p.value,
+                     meta_group = meta) %>%
+      dplyr::select( meta_group, beta, p_value) %>%
+      dplyr::arrange(p_value)
+
+
+
+  }
 
   write.csv(logit_report, paste( df_name, "trt_logitwith_age_gender_race_beta_p_value.csv", sep= "_"))
 
